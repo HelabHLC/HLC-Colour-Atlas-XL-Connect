ef4bef0b5ddb05d9fc5b7f243b09c8024dea1c6e
HelabHLC <norbert.m.woiwod@gmail.com>
2025-03-29 21:35:18 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, send_from_directory
 import os
 
-# Pfad zur index.html im frontend-Ordner
 frontend_folder = os.path.join(os.path.dirname(__file__), '../frontend')
-
 app = Flask(__name__, static_folder=frontend_folder)
 
 @app.route('/')
@@ -11,7 +9,7 @@ def index():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))  # 👈 Heroku-Port verwenden
+    app.run(host='0.0.0.0', port=port)
 
 
