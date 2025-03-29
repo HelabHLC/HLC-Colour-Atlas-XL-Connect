90311e48c457870eb0bb64480ce90aa18a052b16
HelabHLC <norbert.m.woiwod@gmail.com>
2025-03-29 20:42:38 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="../frontend")
 
 @app.route('/')
 def index():
-    return 'HLC Connect läuft!'
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
